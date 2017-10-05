48e7606e2355037ecb089e0ed23b937b27e95b9b
mortalspirit <32247671+mortalspirit@users.noreply.github.com>
2017-10-05 11:59:48 -0400

    Update PCB.java

--- PCB.java
+++ PCB.java
@@ -2,12 +2,12 @@
  * Created by Cam1221 on 10/2/2017.
  */
 public class PCB {
-    public int pid;
-    public int AO;
-    public long AT = System.currentTimeMillis();
-    public int Priority;
-    public String state = "";
-    public static int nextpid = 0;
+    public int pid; //process ID
+    public int AO; //arrivalOrder
+    public long AT = System.currentTimeMillis(); //arrival time
+    public int Priority; 
+    public String state = ""; 
+    public static int nextpid = 0; //location of next process to execute
 
     public PCB(int pid, int AO, int AT, int Priority, String state)
     {
@@ -21,7 +21,8 @@ public class PCB {
 
     public String showPCB()
     {
-        return "P_ID: " + this.getPid() + "\nState: " + this.getState() + "\nArrival Time: " + this.getAT() + "\nArrival Order: " + this.getAO() + "\nPriority: " + this.getPriority() +"\n";
+        return "P_ID: " + this.getPid() + "\nState: " + this.getState() + "\nArrival Time: " + 
+            this.getAT() + "\nArrival Order: " + this.getAO() + "\nPriority: " + this.getPriority() +"\n";
 
     }
 
