ec021a2bc3cac65edc2f9fe436ea8fa68d26c03e
Finton38 <32285866+Finton38@users.noreply.github.com>
2017-10-07 19:31:31 -0400

    Update PCB.java

--- PCB.java
+++ PCB.java
@@ -1,8 +1,6 @@
 public class PCB {
-    public int pid;
-    public int AO;
+    public int pid, AO, Priority;
     public long AT = System.currentTimeMillis();
-    public int Priority;
     public String state = "";
     public static int nextpid = 0;
 
@@ -22,22 +20,54 @@ public class PCB {
 
     }
 
-    public int getPid() {
-        return pid;
-    }
-    public int getAO() {
-        return AO;
-    }
-    public int getPriority(){
-        return Priority;
-    }
-    public long getAT(){
-        return AT;
-    }
+	public int getPid() {
+		return pid;
+	}
 
-    public String getState() {
-        return state;
-    }
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public int getAO() {
+		return AO;
+	}
+
+	public void setAO(int aO) {
+		AO = aO;
+	}
+
+	public int getPriority() {
+		return Priority;
+	}
+
+	public void setPriority(int priority) {
+		Priority = priority;
+	}
+
+	public long getAT() {
+		return AT;
+	}
+
+	public void setAT(long aT) {
+		AT = aT;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public static int getNextpid() {
+		return nextpid;
+	}
+
+	public static void setNextpid(int nextpid) {
+		PCB.nextpid = nextpid;
+	}
+
     //To do: PCB data structure of a process
     //for example: Process_id, Arrive_time, state,
     //PositionOfNextInstructionToExecute(PC value)
