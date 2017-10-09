998a05db192184822f4da87abd2d6deeb64c5345
camm13 <32238879+camm13@users.noreply.github.com>
2017-10-09 18:52:58 -0400

    Newest Update

---
+++ src/CPU.java
@@ -0,0 +1,74 @@
+
+
+
+public class CPU extends Thread {
+    private boolean BusyOrNot = false;
+    public int PC; // Your CPU only has one register PC
+    public int timeslice;
+    private int burstnumber;
+
+    public CPU() {
+
+        //timeslice = settimeslice; only needed for RR
+        //BusyOrNot = false;
+    }
+
+
+    OS ss = new OS();
+
+    Thread thread = new Thread();
+
+    public void run() {
+
+        System.out.println("CPU Thread Running");
+        int burst = getBurstNumber();
+        BusyOrNot = true;
+        for(int i = 0; i < burst; i++)
+        {
+            BubbleSort();
+        }
+
+        BusyOrNot = false;
+
+    }
+
+
+    /**looks like we need a pair and process class
+     * for this method
+     public class Pair< PC, state> execute(Process P){
+
+
+     }
+     */
+
+
+
+    public void BubbleSort(){
+        int a[] = new int[100];
+        int temp = 0;
+        for(int i = 0; i < a.length; i++)
+        {
+            for(int j = 1; j <(a.length-1); j++)
+            {
+                if (a[j] > a[j-1]){
+                    temp = a[j];
+                    a[j]= a[j-1];
+                    a[j-1] = temp;
+                }
+            }
+        }
+    }
+
+    public boolean cpuIsBusy() {
+        return BusyOrNot;
+    }
+    public  void setBurstNumber(int burstnumber) {
+        this.burstnumber = burstnumber;
+    }
+    public int getBurstNumber() {
+
+        return burstnumber;
+
+    }
+}
+
---
+++ src/PCB.java
@@ -0,0 +1,81 @@
+
+public class PCB {
+    public int pid, AO, Priority;
+    public long AT;
+    public State state;
+    public static int nextpid = 0;
+
+    public enum State{
+        New, Ready, Run, Wait, Terminate
+    }
+    public PCB(int pid, int AO, int Priority)
+    {
+        state = State.New;
+        this.pid = ++PCB.nextpid;
+        this.AT = System.currentTimeMillis();
+        this.AO = AO;
+        this.Priority = Priority;
+
+    }
+
+    public String showPCB()
+    {
+        return "P_ID: " + this.getPid() + "\nState: " + this.getState() + "\nArrival Time: " + this.getAT() + "\nArrival Order: " + this.getAO() + "\nPriority: " + this.getPriority() +"\n";
+
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getAO() {
+        return AO;
+    }
+
+    public void setAO(int aO) {
+        AO = aO;
+    }
+
+    public int getPriority() {
+        return Priority;
+    }
+
+    public void setPriority(int priority) {
+        Priority = priority;
+    }
+
+    public long getAT() {
+        return AT;
+    }
+
+    public void setAT(long aT) {
+        AT = aT;
+    }
+
+    public State getState()
+    {
+        return state;
+    }
+
+    public void setState(PCB.State state)
+    {
+        this.state = state;
+    }
+
+    public static int getNextpid() {
+        return nextpid;
+    }
+
+    public static void setNextpid(int nextpid) {
+        PCB.nextpid = nextpid;
+    }
+
+    //To do: PCB data structure of a process
+    //for example: Process_id, Arrive_time, state,
+    //PositionOfNextInstructionToExecute(PC value)
+    //and so on
+}
