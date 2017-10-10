652adc170632f0ae7bb75d45c78ecb21794f8f98
mortalspirit <32247671+mortalspirit@users.noreply.github.com>
2017-10-09 20:58:34 -0400

    Update OS.java
    OS with priority and fcfs algorithms(need RR)

--- src/OS.java
+++ src/OS.java
@@ -1,11 +1,12 @@
 
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 //Add changed variables to fit pcb object
-
+import java.util.Collections;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -22,6 +23,7 @@ public class OS {
 	static ArrayList<ProcessImage> R_Q = new ArrayList<ProcessImage>();
 	static ArrayList<ProcessImage> W_Q = new ArrayList<ProcessImage>();
 	static ArrayList<ProcessImage> T_Q = new ArrayList<ProcessImage>();
+	static ArrayList<Integer> Priority = new ArrayList<Integer>();
 
 	public static void read() throws IOException {
 		try {
@@ -52,14 +54,110 @@ public class OS {
 
 		try {
 			int a = scan.nextInt();
+			if (a == 4) {
+				System.out.println("Terminating Simulator...");
+				System.exit(4);
+			}
+			int processCount = 0;
+			read(); // read all and put into N_Q(N_Q HAS ALL PROCESSES)
+			if (a == 3) { // PRIORITY
+
+				for (int i = 0; i < N_Q.size(); i++) {
+					ProcessImage moveImage = N_Q.get(i);
+					int m = moveImage.getPriority();
+					Priority.add(m);
+
+				}
+
+				System.out.println(Priority.toString());
+
+				while (processCount < N_Q.size()) {
+
+					int minIndex = Priority.indexOf(Collections.min(Priority));
+					System.out.println(Priority.toString());
+
+					System.out.println("Min" + minIndex);
+
+					R_Q.add(N_Q.get(minIndex)); // Start with Ready Queue Add
+
+					ProcessImage pImage = N_Q.get(minIndex); // start with RQ
+					Priority.set(minIndex, 10000);
+
+					PCB B = pImage.getPcb_data(); // PCB obj for getting states,(not sure how to do this)
+					B.setState(PCB.State.Run);
+					if (!W_Q.isEmpty()) {
+						W_Q.remove(0);
+					}
+					B.setState(PCB.State.Ready);
+					R_Q.add(pImage);
+
+					long processArivalTime = System.currentTimeMillis();
+					long responseTimeStart = System.currentTimeMillis();
+					long responseTimeEnd = 0;
+
+					ArrayList<Integer> codeList = pImage.returnCode();
+
+					for (int i = 0; i < codeList.size();) {
+						int currentBurst = codeList.get(i);
+
+						CPU s = new CPU();
+						IODevice k = new IODevice();
+
+						if (s.cpuIsBusy() == false) {
+
+							s.setBurstNumber(currentBurst);
+							s.run();
+							if (i == 0) {
+								responseTimeEnd = System.currentTimeMillis();
+
+							}
+
+							System.out.println("done with burst: " + currentBurst);
+							i++;
+
+						}
+						if (i >= codeList.size()) {
+							break;
+						}
+
+						currentBurst = codeList.get(i);
+						if (s.cpuIsBusy() == false && k.IOIsBusy() == false) {
+							B.setState(PCB.State.Wait);
+							// W_Q.add(N_Q.get(processCount));
+							if (!R_Q.isEmpty()) {
+								W_Q.add(N_Q.get(processCount));
+
+								R_Q.remove(0);
+
+							}
+
+							k.setBurstNumber(currentBurst);
+							k.run();
+
+							System.out.println("done with burst: " + currentBurst);
+
+							i++;
+
+						}
+
+					}
+
+					// ----PROCESS END----
+					long responseTime = responseTimeEnd - responseTimeStart;
+					long endTime = System.currentTimeMillis();
+					long latency = endTime - processArivalTime;
+					System.out.println("Process " + pImage.getpId() + " Latency:" + latency);
+					System.out.println("Process " + pImage.getpId() + " Response Time:" + responseTime);
+
+					B.setState(PCB.State.Terminate);
+					T_Q.add(pImage);
+					processCount++;
+
+				}
+
+			}
+
 			if (a == 1) {
-				int processCount = 0;
-				read(); // read all and put into N_Q(N_Q HAS ALL PROCESSES)
-
-				// --------
-				// Priority algorithm - here you can collection.sort by priority
-				// -------
-
 				while (processCount < N_Q.size()) {
 
 					// W_Q = N_Q; // not sure if this is how wait queue is supposed to be used?
@@ -138,22 +236,14 @@ public class OS {
 					processCount++;
 
 				}
-
-				// add FCFS algorithm
-			} else if (a == 2) {
-				// add RR algorithm
-			} else if (a == 3) {
-				// add SP algorithm
-			} else if (a == 4) {
-				System.out.println("Terminating Simulator...");
-				System.exit(4);
 			}
+
+			// add FCFS algorithm
+
+			// add SP algorithm
+
 		} catch (InputMismatchException e) {
 			System.err.println("Invalid Entry!");
 		}
 	}
 }
-/**
- * Didn't wanna overwrite the OS file we have now if you didnt agree with the
- * changes I made.
- */
